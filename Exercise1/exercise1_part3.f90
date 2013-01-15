b398b34661471acc1c3ecdc1c91331535083664d
Olli Wilkman <olli.wilkman@iki.fi>
2013-01-15 14:14:14 +0200

    Add second equation form to part 3

--- Exercise1/exercise1_part3.f90
+++ Exercise1/exercise1_part3.f90
@@ -3,18 +3,43 @@ program exercise1_3
     implicit none
     double precision delta, epsilon
     double precision x0, x1
+    integer i
     
-    x0 = 1.0 ! Initial guess
     epsilon = 1e-12 ! required precision
-    delta = 1 ! has to be >epsilon at first
     
+    write(*,*) "Using x = log(2 + x^2) form"
     ! Iterate until desired precision
+    x0 = 1.0 ! Initial guess
+    delta = 1 ! has to be >epsilon at first
+    i = 0
     do while (delta > epsilon)
         x1 = log(2 + x0**2)
         delta = abs(x0-x1)
         x0 = x1
+        i = i + 1
     end do
     
     write(*,*) "Found solution:", x0
     write(*,*) "Value at solution (should be zero):", exp(x0)-x0**2-2
+    write(*,*) "Number of iterations:", i
+
+    ! The second method does not converge properly,
+    ! because x1 = sqrt(exp(x0) - 2) easily produces a value which makes
+    ! exp(x1) < 2, and therefore the sqrt produces NaN
+    write(*,*)
+    write(*,*) "Using x = sqrt(exp(x) - 2) form"
+    ! Iterate until desired precision
+    x0 = 2.0 ! Initial guess
+    delta = 1 ! has to be >epsilon at first
+    i = 0
+    do while (delta > epsilon)
+        x1 = sqrt(exp(x0) - 2)
+        delta = abs(x0-x1)
+        x0 = x1
+        i = i + 1
+    end do
+    
+    write(*,*) "Found solution:", x0
+    write(*,*) "Value at solution (should be zero):", exp(x0)-x0**2-2
+    write(*,*) "Number of iterations:", i
 end program
