7ca9870b1a2362cf3f34935fa81fc363615faa84
Olli Wilkman <olli.wilkman@iki.fi>
2013-01-28 13:22:51 +0200

    Add radian conversion to Ex1p2

--- Exercise1/exercise1_part2.f90
+++ Exercise1/exercise1_part2.f90
@@ -3,15 +3,19 @@ program exercise1_2
     ! theta-angle chosen to go from 0 at the positive z-axis
     ! to pi at the negative z-axis
     implicit none
+    real, parameter :: pi = 3.141592654
     real rho, phi, theta
     real x, y, z
     
     write(*,*) "Please enter rho:"
     read(*,*) rho
-    write(*,*) "Please enter phi:"
+    write(*,*) "Please enter phi (degrees):"
     read(*,*) phi
-    write(*,*) "Please enter theta:"
+    write(*,*) "Please enter theta (degrees):"
     read(*,*) theta
+    
+    phi = phi*pi/180
+    theta = theta*pi/180
     
     x = rho * cos(phi) * sin(theta)
     y = rho * sin(phi) * sin(theta)
