89163c9fe6a0e5d4f50469872413226926db22a6
Olli Wilkman <olli.wilkman@iki.fi>
2013-03-17 17:50:01 +0200

    Fix normal distribution CDF

--- Project/src/normal.f90
+++ Project/src/normal.f90
@@ -4,10 +4,10 @@ module ModNormal
 contains
 
 ! Probability distribution function for normal distribution
-real function pdfNorm(x,mu,sigma)
+real function pdfNormal(x,mu,sigma)
     real, intent(in) :: x,mu,sigma
-    pdfNorm = 1.0 / (sqrt(2*pi) * sigma)
-    pdfNorm = pdfNorm * exp(-(x-mu)**2 / (2 * sigma**2))
+    pdfNormal = 1.0 / (sqrt(2*pi) * sigma)
+    pdfNormal = pdfNormal * exp(-(x-mu)**2 / (2 * sigma**2))
 end function
 
 ! Cumulative distribution function for normal distribution
@@ -23,11 +23,11 @@ end function
 ! Approximation of the CDF for a standard normal distribution
 real function cdfStdNormal(x) result (P)
     real, intent(in) :: x
-    real :: t
+    real :: t, y
     t = 1.0 / (1 + 0.33267 * x)
-    P = exp(-x**2 / (2 * sqrt(2*pi)))
-    P = P * (0.4361836*t - 0.1201676*t**2 + 0.9372980*t**3)
-    P = 1 - P
+    y = exp(-x**2 / 2) / sqrt(2*pi)
+    y = y * (0.4361836*t - 0.1201676*t**2 + 0.9372980*t**3)
+    P = 1 - y
 end function
 
 
