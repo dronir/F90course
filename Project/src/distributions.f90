e359393087b4a2371ed23bf12b58eb6dea168868
Olli Wilkman <olli.wilkman@iki.fi>
2013-03-17 15:15:40 +0200

    Add constructors for distributions

--- Project/src/distributions.f90
+++ Project/src/distributions.f90
@@ -1,6 +1,6 @@
 module Distributions
     integer, parameter :: DIST_UNIFORM = 0
-    integer, parameter :: DIST_NORM = 1
+    integer, parameter :: DIST_NORMAL = 1
     integer, parameter :: DIST_CHISQ = 2
     
     type Distribution
@@ -9,14 +9,42 @@ module Distributions
     end type
     
 contains
+    ! Constructors for prettier generation of Distribution type objects
+    ! Uniform constructor
+    type(Distribution) function Uniform(a, b)
+        real, intent(in) :: a,b
+        Uniform%kind = DIST_UNIFORM
+        Uniform%a = a
+        Uniform%b = b
+    end function
+    
+    ! Normal constructor
+    type(Distribution) function Normal(mu, sigma)
+        real, intent(in) :: mu,sigma
+        Normal%kind = DIST_NORMAL
+        Normal%a = mu
+        Normal%b = sigma
+    end function
+    
+    ! Chi-squared constructor
+    type(Distribution) function ChiSq(k)
+        integer, intent(in) :: k
+        ChiSq%kind = DIST_CHISQ
+        ChiSq%a = k
+        ChiSq%b = 0.0
+    end function
+    
+    
     ! Generic probability distribution function
-    real function P(dist, x)
+    real function Pdf(dist, x)
         real, intent(in) :: x
         type(Distribution), intent(in) :: dist
 
         select case (dist%kind)
         case(0)
-            P = pUnif(x, dist%a, dist%b)
+            P = pdfUnif(dist%a, dist%b)
+        case(1)
+            P = pdfNorm(x, dist%a, dist%b)
         case default
             P = 0.0 
         end select
@@ -25,15 +53,26 @@ contains
     
     
     ! Probability distribution function for uniform distribution
-    real function pUnif(x,a,b)
-        real, intent(in) :: x,a, b
-        pUnif = 1.0 / (b-a)
+    ! This would be simple to just inline in the P(dist,x) function,
+    ! but written out for completeness.
+    real function pdfUnif(a,b)
+        real, intent(in) :: a, b
+        pdfUnif = 1.0 / (b-a)
     end function
     
     ! Cumulative distribution function for uniform distribution
-    real function FUnif(x,a,b)
+    real function cdfUnif(x,a,b)
         real, intent(in) :: x,a,b
-        FUnif = min(max((x-a) / (b-a), 0.0), 1.0)
+        cdfUnif = min(max((x-a) / (b-a), 0.0), 1.0)
     end function
+    
+    ! Probability distribution function for normal distribution
+    real function pdfNorm(x,a,b)
+        real, intent(in) :: x,a,b
+        pdfNorm = min(max((x-a) / (b-a), 0.0), 1.0)
+    end function
+    
+    ! Cumulative distribution function for normal distribution
+!    real 
     
 end module
